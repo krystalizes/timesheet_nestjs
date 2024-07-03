64004ffeee76ec174dde246e941e088a164b7425
krystalizes <91723136+krystalizes@users.noreply.github.com>
2024-07-03 12:38:08 +0700

    build(jwt): add store tokens on cookie(may delete token in db later)

--- src/module/auth/common/decorator/get-current-user.decorator.ts
+++ src/module/auth/common/decorator/get-current-user.decorator.ts
@@ -5,6 +5,8 @@ export const GetCurrentUser = createParamDecorator(
   (data: keyof JwtPayloadWithRt | undefined, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
     if (!data) return request.user;
-    return request.user[data];
+    const token = request.cookies['access_token'];
+    const user = token ? (request.user as JwtPayloadWithRt) : null;
+    return user ? user[data] : null;
   },
 );
