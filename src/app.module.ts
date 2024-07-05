e9bc68ba8c6974381cfcc7a5506887c784bb928f
krystalizes <91723136+krystalizes@users.noreply.github.com>
2024-07-05 10:22:47 +0700

    build(multiple_env): add config to run multiple env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,26 @@ import { ProjectModule } from './module/project/project.module';
 import { UserProjectModule } from './module/user_project/user_project.module';
 import { TaskModule } from './module/task/task.module';
 import { TimesheetModule } from './module/timesheet/timesheet.module';
-import { dataSourceOptions } from './config/database/ormConfig';
+// import { dataSourceOptions } from './config/database/ormConfig';
 import { AuthModule } from './module/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AtGuard } from './module/auth/common/guards';
 import { RolesGuard } from './module/auth/common/guards/role.guard';
 import { CloudflareModule } from './module/cloudflare/cloudflare.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmConfigService } from './config/database/TypeOrmConfigService';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(dataSourceOptions),
+    ConfigModule.forRoot({
+      envFilePath: `src/config/env/${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+      inject: [ConfigService],
+    }),
     UserModule,
     ClientModule,
     ProjectModule,
--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
+  const port = process.env.PORT;
   const config = new DocumentBuilder()
     .setTitle('Timesheet API')
-    .setDescription('[Swagger JSON File](http://localhost:3000/api-json)')
+    .setDescription(`[Swagger JSON File](http://localhost:${port}/api-json)`)
     .setVersion('1.0')
     .addTag('Nestjs')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(port);
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
